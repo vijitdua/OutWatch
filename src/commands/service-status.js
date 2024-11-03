75e738c23eb6422a05bd848a2494e9199b9e823b
vijitdua <82555472+vijitdua@users.noreply.github.com>
2024-11-03 14:02:29 +0800

    feat: more details in discordEmbeds, re-runs service check per each command call for discord (not available for get API)

--- src/commands/service-status.js
+++ src/commands/service-status.js
@@ -1,26 +1,23 @@
 // commands/service-status.js
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
 import Service from '../models/serviceModel.js';
+import {trackUptime} from "../service/serviceStatusService.js";
 
 export default {
     data: new SlashCommandBuilder()
         .setName('service-status')
         .setDescription('Displays the status of all services.'),
     async execute(interaction) {
-        console.log('Received /service-status command');
         await interaction.deferReply(); // Defer the reply as fetching data might take time
 
         try {
-            console.log('Fetching services from the database...');
+            await trackUptime();
             const services = await Service.findAll();
-            console.log(`Fetched ${services.length} services.`);
 
             if (services.length === 0) {
-                console.log('No services found.');
                 return interaction.editReply('No services found.');
             }
 
-            console.log('Building embed message...');
             const embed = new EmbedBuilder()
                 .setTitle('📊 Service Status')
                 .setColor(0x00AE86)
@@ -29,18 +26,20 @@ export default {
             services.forEach(service => {
                 const statusEmoji = service.status === 'up' ? '🟢' : service.status === 'down' ? '🔴' : '⚪️';
                 const downSince = service.downSince ? new Date(service.downSince).toLocaleString() : 'N/A';
+                const lastChecked = new Date(service.lastChecked).toLocaleString();
+                let serviceInfo = `**Status**: ${service.status.toUpperCase()}`;
+                serviceInfo += service.downSince ? `\n**Down Since**: ${downSince}` : ``
+                serviceInfo += `\n**Last Checked**: ${lastChecked}`;
+                serviceInfo += `\nURL: ${service.url}`
                 embed.addFields({
                     name: `${statusEmoji} ${service.serviceName}`,
-                    value: `**Status**: ${service.status.toUpperCase()}\n**Down Since**: ${downSince}`,
+                    value: serviceInfo,
                     inline: false,
                 });
             });
 
-            console.log('Sending embed message...');
             await interaction.editReply({ embeds: [embed] });
-            console.log('Embed message sent successfully.');
         } catch (error) {
-            console.error('Error in /service-status command:', error);
             await interaction.editReply('There was an error fetching the service statuses.');
         }
     },
